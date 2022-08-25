c1e0d72e66fe16806f6bc601cb234f3a099aff21
dommilosz <dommilosz@gmail.com>
2022-08-25 13:24:19 +0200

    add tests for non-existing config file, add details in package.json

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,9 @@
 import configured from "../index";
 import fs from "fs";
 
-const configFile = "./src/tests/read.json"
+//Unmodified files will have spaces in their content
 const configFileContent = '{"a":{"b":{"c":{"d":"overwritten"} } } }'
+
 const defaultConfig = {
     a: {
         b: {
@@ -10,8 +11,8 @@ const defaultConfig = {
                 d: "d-default"
             }
         },
-        b2:{
-            c:"c-default"
+        b2: {
+            c: "c-default"
         }
     }
 }
@@ -23,14 +24,59 @@ const configAfterMerge = {
                 d: "overwritten"
             }
         },
-        b2:{
-            c:"c-default"
+        b2: {
+            c: "c-default"
         }
     }
 }
 
-describe('reading config',()=>{
-    test('prepare the config file for reading',()=>{
+describe('reading the non existing config', () => {
+    const configFile = "./src/tests/read1.json"
+    test("check to ensure the config doesn't exist", () => {
+        if (fs.existsSync(configFile))
+            fs.unlinkSync(configFile);
+        expect(fs.existsSync(configFile)).toBeFalsy();
+    });
+
+    test('read the config file', () => {
+        const config = configured({path: configFile, writeMissing: false, defaultConfig: defaultConfig});
+
+        expect(config).toBeDefined();
+        expect(config.a.b.c.d).toBe("d-default");
+        expect(config.a.b2.c).toBe("c-default");
+    });
+
+    test("check to ensure the config wasn't created", () => {
+        expect(fs.existsSync(configFile)).toBeFalsy();
+    });
+});
+
+describe('reading the non existing config with writeMissing turned on', () => {
+    const configFile = "./src/tests/read2.json"
+    test("check to ensure the config doesn't exist", () => {
+        if (fs.existsSync(configFile))
+            fs.unlinkSync(configFile);
+        expect(fs.existsSync(configFile)).toBeFalsy();
+    });
+
+    test('read the config file', () => {
+        const config = configured({path: configFile, writeMissing: true, defaultConfig: defaultConfig});
+
+        expect(config).toBeDefined();
+        expect(config.a.b.c.d).toBe("d-default");
+        expect(config.a.b2.c).toBe("c-default");
+    });
+
+    test("check to ensure the config wasn't created", () => {
+        expect(fs.existsSync(configFile)).toBeTruthy();
+        let file = fs.readFileSync(configFile, {encoding: "utf-8"});
+        expect(JSON.stringify(JSON.parse(file))).toEqual(JSON.stringify(defaultConfig))
+    });
+})
+
+describe('reading the config', () => {
+    const configFile = "./src/tests/read3.json"
+    test('prepare the config file for reading', () => {
         fs.writeFileSync(configFile, configFileContent, {encoding: "utf-8"});
     });
 
@@ -42,14 +88,15 @@ describe('reading config',()=>{
         expect(config.a.b2.c).toBe("c-default");
     });
 
-    test('test to check is the file not changed',()=>{
-        let file = fs.readFileSync(configFile,{encoding:"utf-8"});
+    test('test to check is the file not changed', () => {
+        let file = fs.readFileSync(configFile, {encoding: "utf-8"});
         expect(file).toEqual(configFileContent);
     })
 })
 
-describe('reading config with writeMissing turned on',()=>{
-    test('prepare the config file for reading',()=>{
+describe('reading the config with writeMissing turned on', () => {
+    const configFile = "./src/tests/read4.json"
+    test('prepare the config file for reading', () => {
         fs.writeFileSync(configFile, configFileContent, {encoding: "utf-8"});
     });
 
@@ -61,8 +108,8 @@ describe('reading config with writeMissing turned on',()=>{
         expect(config.a.b2.c).toBe("c-default");
     });
 
-    test('test to check is the file changed',()=>{
-        let file = fs.readFileSync(configFile,{encoding:"utf-8"});
+    test('test to check is the file changed', () => {
+        let file = fs.readFileSync(configFile, {encoding: "utf-8"});
         expect(file).not.toEqual(configFileContent);
         expect(JSON.stringify(JSON.parse(file))).toEqual(JSON.stringify(configAfterMerge))
     })
